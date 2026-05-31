62982eefc123ab1f7b10525534362c63bac22fdc
Toadzi <170478014+Toadzi@users.noreply.github.com>
2026-05-31 11:56:56 +0200

    Example Config

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Deployment-Konfiguration – Vorlage
+ * Kopiere diese Datei zu config.php und passe die Werte an.
+ */
+
+// Unterverzeichnis-Konfiguration:
+// '' für Root-Installation (https://example.com/)
+// '/tagesmutter' für Unterverzeichnis (https://example.com/tagesmutter/)
+define('APP_BASE_PATH', '');
